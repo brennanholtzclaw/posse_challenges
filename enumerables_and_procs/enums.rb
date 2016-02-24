ddb598b9aba4d45913eab2962197eb4c52ac3a13
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-02-24 07:29:21 -0700

    functional - adding zip next

--- enumerables_and_procs/enums.rb
+++ enumerables_and_procs/enums.rb
@@ -16,4 +16,23 @@ class Enums
     result
   end
 
+  def self.reduce(collection, condition)
+    result = []
+    collection.each do |elem|
+      result << condition.call(elem)
+    end
+    result
+  end
+
+  def self.reject(collection, condition)
+    result = []
+    collection.each do |elem|
+      result << elem if condition.call(elem)
+    end
+    result
+  end
+
+  def self.zip(collection)
+    
+  end
 end
